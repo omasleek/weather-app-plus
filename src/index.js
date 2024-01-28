a2e264f75d06b3506ad6e9a312e54eba4829b066
Chioma ogbonna <polichilda66@gmail.com>
2024-01-28 16:18:45 +0100

    Replaced the demo content with real life content

--- src/index.js
+++ src/index.js
@@ -52,6 +52,11 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[date.getDay()];
+}
 
 function getForecast(city) {
   let apiKey = "153ee0af401ee03o5bae93d2e6a4708t";
@@ -59,25 +64,29 @@ function getForecast(city) {
   axios(apiUrl).then(displayForecast);
 }
 
-function displayForecast() {
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+function displayForecast(response) {
+  console.log(response.data);
+
+
   let forecastHtml ="";
   
-days.forEach(function (day) {
+response.data.daily.forEach(function (day, index) {
+  if (index <5) {
   forecastHtml =
   forecastHtml +
   `
   <div class="weather-forecast-day">
-          <div class="weather-forecast-date">${day}</div>
-          <div class="weather-forecast-icon">🌤️</div>
+          <div class="weather-forecast-date">${formatDay(day.time)}</div>
+          <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
           <div class="weather-forecast-temperatures">
             <div class="weather-forecast-temperature">
-              <strong>15º</strong>
+              <strong>${Math.round(day.temperature.maximum)}º</strong>
             </div>
-            <div class="weather-forecast-temperature">9º</div>
+            <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}º</div>
           </div>
         </div>
       `;
+    }
 });
 
 let forecastElement = document.querySelector("#forecast");
